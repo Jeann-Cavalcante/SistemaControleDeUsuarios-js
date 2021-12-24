246316ec2cfb0b10a14780d96c8c8a7cea7c0185
Jean <jeanncavalcante@gmail.com>
2021-12-23 23:03:48 -0300

    Versão 1.0.1

---
+++ js/app.js
@@ -0,0 +1,1 @@
+let userControllers = new UserController();
---
+++ js/models/User.js
@@ -0,0 +1,43 @@
+class User {
+  constructor(id, name, photo, email, admin, password) {
+    this._id = id;
+    this._name = name;
+    this._photo = photo;
+    this._email = email;
+    this._admin = admin;
+    this._password = password;
+    let date = new Date();
+    this._date =
+      date.toLocaleDateString("pt-BR") +
+      " " +
+      date.toLocaleDateString("pt-BR").slice(0, 5);
+  }
+
+  getId() {
+    return this._id;
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  getPhoto() {
+    return this._photo;
+  }
+
+  getEmail() {
+    return this._email;
+  }
+
+  getAdmin() {
+    return this._admin;
+  }
+
+  getPassword() {
+    return this._password;
+  }
+
+  getDate() {
+    return this._date;
+  }
+}
